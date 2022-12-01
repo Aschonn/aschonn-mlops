d4d4192c177fc3801405a800b03b766643ed9715
Aschonn <aschonn_trinity@yahoo.com>
2022-12-01 13:35:39 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "src",
+    version = "0.0.1",
+    description="computer vision version 1 test",
+    author="aschonn",
+    packages=find_packages(),
+    license="MIT",
+)
---
+++ tests/test_config.py
@@ -0,0 +1,7 @@
+#has to start with test
+#Creates a function to test a specific feature
+
+def test_whatever():
+    a = 2
+    b = 2
+    assert a == b
